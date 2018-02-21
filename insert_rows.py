8ba891119723f3becb067c16f4d94da038429598
Aniket Sakinala <sakihenry@gmail.com>
2018-02-20 19:59:36 -0600

    Add files via upload

--- insert_rows.py
+++ insert_rows.py
@@ -1,5 +1,6 @@
 import json
 import os
+from copy import deepcopy
 from datetime import date, time
 
 import mysql.connector
@@ -15,38 +16,15 @@ cursor = cnx.cursor()
 
 
 def insert_actor(object):
-    actor = object['actor']
-    rows = {'id': 'idactor', 'objectType': 'objectType', 'link': 'link', 'displayName': 'displayName',
-            'postedDate': 'postedDate', 'postedTime': 'postedTime', 'image': 'image', 'summary': 'summary',
-            'friendsCount': 'friendsCount', 'followersCount': 'followersCount', 'listedCount': 'listedCount',
-            'statusesCount': 'statusesCount', 'favoritesCount': 'favoritesCount', 'twitterTimeZone': 'timezone',
-            'verified': 'verified', 'utcOffset': 'utcOffset', 'preferredUsername': 'preferredUsername',
-            'location': 'location'}
-    attributes = sorted(list(rows.keys() & actor.keys()))
-    actor['id'] = int(actor['id'].split(':')[2])
+    actor = deepcopy(object['actor'])
     if 'links' in actor:
         del actor['links']
     if 'languages' in actor:
         del actor['languages']
-    if 'postedTime' in attributes:
-        a, b = actor['postedTime'].split('T')
-        y, m, d = a.split('-')
-        h, mi, s = b.split('.')[0].split(':')
-        actor['postedDate'] = date(int(y), int(m), int(d))
-        actor['postedTime'] = time(int(h), int(mi), int(s))
-        attributes.insert(attributes.index('postedTime'), 'postedDate')
-        del a, b, y, m, d, h, mi, s
-    if 'location' in attributes:
-        actor['location'] = actor['location']['displayName']
-    if 'verified' in attributes:
-        if actor['verified']:
-            actor['verified'] = 1
-        else:
-            actor['verified'] = 0
     query = 'INSERT INTO actor ('
     params = 'VALUES ('
-    for attribute in attributes:
-        query = query + rows[attribute] + ', '
+    for attribute in actor.keys():
+        query = query + attribute + ', '
         params = params + '%(' + attribute + ')s, '
     query = query.strip(', ')
     query = query + ') '
@@ -54,7 +32,6 @@ def insert_actor(object):
     params = params + ')'
     try:
         cursor.execute(query + params, actor)
-        cnx.commit()
     except mysql.connector.ProgrammingError as pe:
         print(pe.msg)
     except mysql.connector.DatabaseError as de:
@@ -62,40 +39,19 @@ def insert_actor(object):
 
 
 def insert_tweet(object):
-    object['id'] = int(object['id'].split(':')[2])
-    object['idactor'] = int(object['actor']['id'].split(':')[2])
-    object['generator_name'] = object['generator']['displayName']
-    object['generator_link'] = object['generator']['link']
-    object['provider_name'] = object['provider']['displayName']
-    object['provider_link'] = object['provider']['link']
-    object['gnip_lang'] = object['gnip']['language']['value']
-    rows = {'id': 'idtweet', 'objectType': 'objectType', 'verb': 'verb', 'postedDate': 'postedDate',
-            'postedTime': 'postedTime', 'link': 'link', 'body': 'body', 'favoritesCount': 'favoritesCount',
-            'twitter_filter_level': 'filter_level', 'twitter_lang': 'twitter_lang', 'retweetCount': 'retweetCount',
-            'idactor': 'idactor', 'generator_name': 'generator_name', 'generator_link': 'generator_link',
-            'provider_name': 'provider_name', 'provider_link': 'provider_link', 'gnip_lang': 'gnip_lang'}
-    attributes = sorted(list(rows.keys() & object.keys()))
-    if 'postedTime' in attributes:
-        a, b = object['postedTime'].split('T')
-        y, m, d = a.split('-')
-        h, mi, s = b.split('.')[0].split(':')
-        object['postedDate'] = date(int(y), int(m), int(d))
-        object['postedTime'] = time(int(h), int(mi), int(s))
-        attributes.insert(attributes.index('postedTime'), 'postedDate')
-        del a, b, y, m, d, h, mi, s
-    del object['actor'], object['generator'], object['provider'], object['object'], object['twitter_entities']
-    del object['gnip']
+    our_object = deepcopy(object)
+    del our_object['actor'], our_object['object'], our_object['gnip'], our_object['twitter_entities']
     query = 'INSERT INTO tweet ('
     params = 'VALUES ('
-    for attribute in attributes:
-        query = query + rows[attribute] + ', '
+    for attribute in our_object.keys():
+        query = query + attribute + ', '
         params = params + '%(' + attribute + ')s, '
     query = query.strip(', ')
     query = query + ') '
     params = params.strip(', ')
     params = params + ')'
     try:
-        cursor.execute(query + params, object)
+        cursor.execute(query + params, our_object)
     except mysql.connector.ProgrammingError as pe:
         print(pe.msg)
     except mysql.connector.DatabaseError as de:
@@ -103,33 +59,21 @@ def insert_tweet(object):
 
 
 def insert_object(object):
-    object['object']['idtweet'] = int(object['id'].split(':')[2])
-    object = object['object']
-    object['id'] = int(object['id'].split(':')[2])
-    rows = {'id': 'idobject', 'objectType': 'objectType', 'postedDate': 'postedDate', 'postedTime': 'postedTime',
-            'link': 'link', 'body': 'summary', 'idtweet': 'idtweet', 'summary': 'summary'}
-    attributes = sorted(list(rows.keys() & object.keys()))
-    if 'postedTime' in attributes:
-        a, b = object['postedTime'].split('T')
-        y, m, d = a.split('-')
-        h, mi, s = b.split('.')[0].split(':')
-        object['postedDate'] = date(int(y), int(m), int(d))
-        object['postedTime'] = time(int(h), int(mi), int(s))
-        attributes.insert(attributes.index('postedTime'), 'postedDate')
-        del a, b, y, m, d, h, mi, s
-    for k in (object.keys() - attributes):
-        del object[k]
+    our_object = deepcopy(object['object'])
+    rows = ['idobject', 'objectType', 'postedDate', 'postedTime', 'link', 'idtweet', 'summary']
+    for k in (our_object.keys() - rows):
+        del our_object[k]
     query = 'INSERT INTO object ('
     params = 'VALUES ('
-    for attribute in attributes:
-        query = query + rows[attribute] + ', '
+    for attribute in our_object.keys():
+        query = query + attribute + ', '
         params = params + '%(' + attribute + ')s, '
     query = query.strip(', ')
     query = query + ') '
     params = params.strip(', ')
     params = params + ')'
     try:
-        cursor.execute(query + params, object)
+        cursor.execute(query + params, our_object)
     except mysql.connector.ProgrammingError as err:
         print(err.msg)
     except mysql.connector.DatabaseError as err:
@@ -137,36 +81,15 @@ def insert_object(object):
 
 
 def insert_actor_links(object):
-    idactor = int(object['actor']['id'].split(':')[2])
     links = object['actor']['links']
-    del object
-    query = 'INSERT INTO actor_links ('
-    params = 'VALUES ('
-    link = links.pop()
-    link['idactor'] = idactor
-    rows = {'idactor': 'idactor', 'href': 'href', 'rel': 'rel'}
-    attributes = sorted(list(rows.keys() & link.keys()))
-    object = []
-    temp = []
-    for attribute in attributes:
-        temp.append(link[attribute])
-        query = query + rows[attribute] + ', '
-        params = params + '%s, '
-    object.append(tuple(temp))
-    query = query.strip(', ')
-    query = query + ') '
-    params = params.strip(', ')
-    params = params + ')'
-    while len(links) > 0:
-        link = links.pop()
-        link['idactor'] = idactor
-        attributes = sorted(list(rows.keys() & link.keys()))
-        temp = []
-        for attribute in attributes:
-            temp.append(link[attribute])
-        object.append(tuple(temp))
+    idactor = object['actor']['idactor']
+    query = 'INSERT INTO actor_links (idactor, href, rel)'
+    params = 'VALUES (%s, %s, %s)'
+    data = []
+    for link in links:
+        data.append((idactor, link['href'], link['rel']))
     try:
-        cursor.executemany(query + params, object)
+        cursor.executemany(query + params, data)
     except mysql.connector.ProgrammingError as pe:
         print(pe.msg)
     except mysql.connector.DatabaseError as de:
@@ -174,16 +97,15 @@ def insert_actor_links(object):
 
 
 def insert_actor_lang(object):
-    idactor = int(object['actor']['id'].split(':')[2])
     langs = object['actor']['languages']
-    del object
+    idactor = object['actor']['idactor']
     query = 'INSERT INTO actor_lang (idactor, language)'
     params = 'VALUES (%s, %s)'
-    object = []
-    for l in langs:
-        object.append((idactor, l))
+    data = []
+    for lang in langs:
+        data.append((idactor, lang))
     try:
-        cursor.executemany(query + params, object)
+        cursor.executemany(query + params, data)
     except mysql.connector.ProgrammingError as pe:
         print(pe.msg)
     except mysql.connector.DatabaseError as de:
@@ -191,16 +113,15 @@ def insert_actor_lang(object):
 
 
 def insert_gnip(object):
-    idtweet = int(object['id'].split(':')[2])
     rules = object['gnip']['matching_rules']
-    del object
+    idtweet = object['idtweet']
     query = 'INSERT INTO gnip (idtweet, value, tag)'
     params = 'VALUES (%s, %s, %s)'
-    object = []
+    data = []
     for rule in rules:
-        object.append((idtweet, rule['value'], rule['tag']))
+        data.append((idtweet, rule['value'], rule['tag']))
     try:
-        cursor.executemany(query + params, object)
+        cursor.executemany(query + params, data)
     except mysql.connector.ProgrammingError as pe:
         print(pe.msg)
     except mysql.connector.DatabaseError as de:
@@ -208,16 +129,15 @@ def insert_gnip(object):
 
 
 def insert_hashtags(object):
-    idtweet = int(object['id'].split(':')[2])
-    del object
     hashtags = object['twitter_entities']['hashtags']
+    idtweet = object['idtweet']
     query = 'INSERT INTO hashtags (idtweet, text, start_index, end_index)'
     params = 'VALUES (%s, %s, %s, %s)'
-    object = []
+    data = []
     for hashtag in hashtags:
-        object.append((idtweet, hashtag['text'], hashtag['indices'][0], hashtag['indices'][1]))
+        data.append((idtweet, hashtag['text'], hashtag['indices'][0], hashtag['indices'][1]))
     try:
-        cursor.executemany(query + params, object)
+        cursor.executemany(query + params, data)
     except mysql.connector.ProgrammingError as pe:
         print(pe.msg)
     except mysql.connector.DatabaseError as de:
@@ -225,16 +145,15 @@ def insert_hashtags(object):
 
 
 def insert_symbols(object):
-    idtweet = int(object['id'].split(':')[2])
-    del object
     symbols = object['twitter_entities']['symbols']
+    idtweet = object['idtweet']
     query = 'INSERT INTO symbols (idtweet, text, start_index, end_index)'
     params = 'VALUES (%s, %s, %s, %s)'
-    object = []
+    data = []
     for symbol in symbols:
-        object.append((idtweet, symbol['text'], symbol['indices'][0], symbol['indices'][1]))
+        data.append((idtweet, symbol['text'], symbol['indices'][0], symbol['indices'][1]))
     try:
-        cursor.executemany(query + params, object)
+        cursor.executemany(query + params, data)
     except mysql.connector.ProgrammingError as pe:
         print(pe.msg)
     except mysql.connector.DatabaseError as de:
@@ -242,17 +161,16 @@ def insert_symbols(object):
 
 
 def insert_urls(object):
-    idtweet = int(object['id'].split(':')[2])
-    del object
+    idtweet = object['idtweet']
     urls = object['twitter_entities']['urls']
     query = 'INSERT INTO urls (idtweet, url, expanded_url, display_url, start_index, end_index)'
     params = 'VALUES (%s, %s, %s, %s, %s, %s)'
-    object = []
+    data = []
     for url in urls:
-        object.append((idtweet, url['url'], url['expanded_url'], url['display_url'], url['indices'][0],
-                       url['indices'][1]))
+        data.append((idtweet, url['url'], url['expanded_url'], url['display_url'], url['indices'][0],
+                     url['indices'][1]))
     try:
-        cursor.executemany(query + params, object)
+        cursor.executemany(query + params, data)
     except mysql.connector.ProgrammingError as pe:
         print(pe.msg)
     except mysql.connector.DatabaseError as de:
@@ -260,17 +178,16 @@ def insert_urls(object):
 
 
 def insert_mentions(object):
-    idtweet = int(object['id'].split(':')[2])
-    del object
+    idtweet = object['idtweet']
     mentions = object['twitter_entities']['user_mentions']
     query = 'INSERT INTO mentions (idtweet, screen_name, name, user_id, start_index, end_index)'
     params = 'VALUES (%s, %s, %s, %s, %s, %s)'
-    object = []
+    data = []
     for mention in mentions:
-        object.append((idtweet, mention['screen_name'], mention['name'], mention['id'], mention['indices'][0],
-                       mention['indices'][1]))
+        data.append((idtweet, mention['screen_name'], mention['name'], mention['id'], mention['indices'][0],
+                     mention['indices'][1]))
     try:
-        cursor.executemany(query + params, object)
+        cursor.executemany(query + params, data)
     except mysql.connector.ProgrammingError as pe:
         print(pe.msg)
     except mysql.connector.DatabaseError as de:
@@ -278,43 +195,94 @@ def insert_mentions(object):
 
 
 def insert_media(object):
-    idtweet = int(object['id'].split(':')[2])
-    del object
+    idtweet = object['idtweet']
     media = object['twitter_entities']['media']
     query = 'INSERT INTO media (idmedia, idtweet, media_url, media_url_https, url, display_url, expanded_url, type, ' \
             ' start_index, end_index)'
     params = 'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
-    object = []
+    data = []
     for medium in media:
-        object.append((medium['id'], idtweet, medium['media_url'], medium['media_url_https'], medium['url'],
-                       medium['display_url'], medium['expanded_url'], medium['type'], medium['indices'][0],
-                       medium['indices'][1]))
-        insert_media_sizes(medium['sizes'], idtweet)
+        data.append((medium['id'], idtweet, medium['media_url'], medium['media_url_https'], medium['url'],
+                     medium['display_url'], medium['expanded_url'], medium['type'], medium['indices'][0],
+                     medium['indices'][1]))
     try:
-        cursor.executemany(query + params, object)
+        cursor.executemany(query + params, data)
     except mysql.connector.ProgrammingError as pe:
         print(pe.msg)
     except mysql.connector.DatabaseError as de:
         print(de.msg)
 
 
-def insert_media_sizes(object, id):
+def insert_media_sizes(object):
+    media = object['twitter_entities']['media']
     query = 'INSERT INTO media_sizes (idmedia, size, width, height, resize)'
     params = 'VALUES (%s, %s, %s, %s, %s)'
-    temp = []
-    for key in object.keys():
-        temp.append((str(id), key, object[key]['width'], object[key]['height'], object[key]['resize']))
+    data = []
+    for medium in media:
+        idmedia = medium['id']
+        for key in medium['sizes'].keys():
+            data.append((idmedia, key, medium['sizes'][key]['w'], medium['sizes'][key]['h'],
+                         medium['sizes'][key]['resize']))
+        try:
+            cursor.executemany(query + params, data)
+        except mysql.connector.ProgrammingError as err:
+            print(err.msg)
+        except mysql.connector.DatabaseError as err:
+            print(err.msg)
+
+
+def format_object(object):
     try:
-        cursor.execute(query + params, temp)
-    except mysql.connector.ProgrammingError as err:
-        print(err.msg)
-    except mysql.connector.DatabaseError as err:
-        print(err.msg)
-
-
-def format_object(json_tweet):
-
-    pass
+        if 'postedTime' in object:
+            a, b = object['postedTime'].split('T')
+            y, m, d = a.split('-')
+            object['postedDate'] = date(int(y), int(m), int(d))
+            y, m, d = b.split('.')[0].split(':')
+            object['postedTime'] = time(int(y), int(m), int(d))
+            del a, b, y, m, d
+        if 'postedTime' in object['actor']:
+            a, b = object['actor']['postedTime'].split('T')
+            y, m, d = a.split('-')
+            object['actor']['postedDate'] = date(int(y), int(m), int(d))
+            y, m, d = b.split('.')[0].split(':')
+            object['actor']['postedTime'] = time(int(y), int(m), int(d))
+            del a, b, y, m, d
+        if 'postedTime' in object['object']:
+            a, b = object['object']['postedTime'].split('T')
+            y, m, d = a.split('-')
+            object['object']['postedDate'] = date(int(y), int(m), int(d))
+            y, m, d = b.split('.')[0].split(':')
+            object['object']['postedTime'] = time(int(y), int(m), int(d))
+            del a, b, y, m, d
+        object['object']['idtweet'] = object['idtweet'] = int(object['id'].split(':')[2])
+        del object['id']
+        object['filter_level'] = object['twitter_filter_level']
+        del object['twitter_filter_level']
+        object['generator_name'] = object['generator']['displayName']
+        object['generator_link'] = object['generator']['link']
+        del object['generator']
+        object['provider_name'] = object['provider']['displayName']
+        object['provider_link'] = object['provider']['link']
+        del object['provider']
+        object['gnip_lang'] = object['gnip']['language']['value']
+        object['idactor'] = object['actor']['idactor'] = int(object['actor']['id'].split(':')[2])
+        del object['actor']['id']
+        if 'location' in object['actor']:
+            object['actor']['location'] = object['actor']['location']['displayName']
+        object['actor']['timezone'] = object['actor']['twitterTimeZone']
+        del object['actor']['twitterTimeZone']
+        if 'verified' in object['actor']:
+            if object['actor']['verified']:
+                object['actor']['verified'] = 1
+            else:
+                object['actor']['verified'] = 0
+        object['object']['idobject'] = int(object['object']['id'].split(':')[2])
+        del object['object']['id']
+        if 'body' in object['object']:
+            object['object']['summary'] = object['object']['body']
+            del object['object']['body']
+    except KeyError as ke:
+        print(ke)
 
 
 def unpack_json(filename):
@@ -332,9 +300,9 @@ def unpack_json(filename):
                         format_object(json_tweet)
                         insert_actor(json_tweet)
                         insert_tweet(json_tweet)
+                        insert_object(json_tweet)
                         insert_actor_links(json_tweet)
                         insert_actor_lang(json_tweet)
-                        insert_object(json_tweet)
                         insert_gnip(json_tweet)
                         if len(json_tweet['twitter_entities']['hashtags']) > 0:
                             insert_hashtags(json_tweet)
@@ -346,11 +314,15 @@ def unpack_json(filename):
                             insert_mentions(json_tweet)
                         if 'media' in json_tweet['twitter_entities']:
                             insert_media(json_tweet)
+                            insert_media_sizes(json_tweet)
                         cnx.commit()
     except UnicodeDecodeError as er:
         print(er)
+    except mysql.connector.DatabaseError as de:
+        print(de.msg)
 
 
 for file in os.listdir(directory):
     filepath = os.path.join(directory, file)
     unpack_json(filepath)
+    print('completed: ', filepath)
