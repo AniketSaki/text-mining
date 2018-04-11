dfaa214cbd19e0ae60900c3c3090823225c3940f
Aniket Sakinala <sakihenry@gmail.com>
2018-04-11 10:05:44 -0500

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,25 @@
+import re
+import string
+from nltk.corpus import stopwords
+sw = set(stopwords.words('english'))
+
+
+def hashtag(text):
+    return re.sub('#[a-zA-Z0-9]+', '', text)
+
+
+def mention(text):
+    return re.sub('@[a-zA-Z0-9]+', '', text)
+
+
+def url(text):
+    return re.sub('http[s]?:[\S]+', '', text)
+
+
+def punctuation(text):
+    exclude = set(string.punctuation)
+    return ''.join(ch for ch in text if ch not in exclude)
+
+
+def stopword(text):
+    return [i for i in text.lower().split() if i not in sw]
