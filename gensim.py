1bd8f0c8115b7131b103c56729b6d4faf978a2a6
Aniket Sakinala <sakihenry@gmail.com>
2018-04-11 10:23:38 -0500

    Add files via upload

--- gensim.py
+++ gensim.py
@@ -1,21 +1,24 @@
-from gensim import models, matutils
-import mysql.connector
-from Code.preprocess import *
-import numpy
 import pickle
+
+import mysql.connector
+import numpy
+from gensim import models, matutils
+
+from Code.preprocess import *
 
 
 def update_matrix(i, j):
-    vectors.append((vectors[i] + vectors[j]) / 2)
-    del cos_sim[i], cos_sim[j]
+    ids.append([ids[i], ids[j]])
+    vectors.append(matutils.unitvec(numpy.mean([vectors[i], vectors[j]], axis=0)))
+    del ids[i], ids[j], cos_sim[i], cos_sim[j]
     cos_sim.append([])
-    for i in range(0, len(vectors)-1):
+    for i in range(0, len(vectors) - 1):
         cos_sim[-1].append(numpy.dot(vectors[-1], vectors[i]))
 
 
 # initiate variables
 vectors = []
-tokens = []
+ids = []
 cos_sim = [[]]
 vocab = [0, 0]
 model = models.KeyedVectors.load_word2vec_format(
@@ -26,35 +29,36 @@ database = 'twitter'
 host = '127.0.0.1'
 cnx = mysql.connector.connect(user=user, password=password, host=host, database=database)
 cursor = cnx.cursor()
-query = "select body from tweet where twitter_lang='en'"
+query = "select idtweet, body from tweet where twitter_lang='en'"
 cursor.execute(query)
 i = 0
 
 # store tokens and calculate cosine similarity matrix
 for record in cursor:
-    tweet = record[0]
+    ids.append([record[0]])
+    tweet = record[1]
     tweet = hashtag(tweet)
     tweet = mention(tweet)
     tweet = url(tweet)
     tweet = punctuation(tweet)
     words = stopword(tweet)
-    tokens.append(words)
     v1 = [model[word] for word in words if word in model.vocab]
     vectors.append(matutils.unitvec(numpy.mean(v1, axis=0)))
-    for j in range(0, len(tokens) - 1):
+    for j in range(0, len(vectors) - 1):
         cos_sim[i].append(numpy.dot(vectors[i], vectors[j]))
     i += 1
     cos_sim.append([])
 # store to file
 with open('cos_sim.pkl', 'wb') as cm:
     pickle.dump(cos_sim, cm)
-with open('words.pkl', 'wb') as wf:
-    pickle.dump(tokens, wf)
+with open('vectors.pkl', 'wb') as wf:
+    pickle.dump(vectors, wf)
 
 # get max value from dot product
-i = numpy.argmax(cos_sim)
-j = numpy.argmax(cos_sim[i])
+while len(cos_sim) > 1:
+    i = numpy.argmax(cos_sim)
+    j = numpy.argmax(cos_sim[i])
+    update_matrix(i, j)
 
-update_matrix(i, j)
 cursor.close()
 cnx.close()
